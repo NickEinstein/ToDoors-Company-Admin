5f0a72ce55eb45936567d6f08e8ebef24c72bf6c
unknown <awonusiolajide@yahoo.com>
2023-03-25 14:20:17 +0100

    updated build

---
+++ build/static/js/425.8a4bea82.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktodoors_web=self.webpackChunktodoors_web||[]).push([[425],{7425:function(t,a,n){n.d(a,{jW7:function(){return e}});var r=n(9983);function e(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M10.828 12l4.95 4.95-1.414 1.414L8 12l6.364-6.364 1.414 1.414z"}}]}]})(t)}}}]);
+//# sourceMappingURL=425.8a4bea82.chunk.js.map
---
+++ build/static/js/820.ba1f8687.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktodoors_web=self.webpackChunktodoors_web||[]).push([[820],{8820:function(t,c,e){e.d(c,{mGl:function(){return o}});var n=e(9983);function o(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-.7 5.2-2L869 536.2a32.07 32.07 0 0 0 0-48.4z"}}]})(t)}}}]);
+//# sourceMappingURL=820.ba1f8687.chunk.js.map
